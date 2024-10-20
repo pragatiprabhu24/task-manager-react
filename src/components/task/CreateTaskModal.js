19a882266f3c22cd132fc407dd1c873888794ebc
pragatiprabhu24 <prabhupragati5@gmail.com>
2024-10-21 02:27:33 +0530

    commmit

--- src/components/task/CreateTaskModal.js
+++ src/components/task/CreateTaskModal.js
@@ -29,7 +29,7 @@ const CreateTaskModal = ({
 }) => {
   const [taskName, setTaskName] = useState("");
   const [taskDescription, setTaskDescription] = useState("");
-  const [taskStatus, setTaskStatus] = useState("pending");
+  const [taskStatus, setTaskStatus] = useState("");
   const [dueDate, setDueDate] = useState("");
   const [category, setCategory] = useState("");
   const [loading, setLoading] = useState(false);
@@ -176,7 +176,7 @@ const CreateTaskModal = ({
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
               value={category}
               onChange={(e) => setCategory(e.target.value)}
-              required
+           
             >
               <option value="" disabled>
                 Select a category
